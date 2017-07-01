33c78d7d3e00609c5cdb6c97f0db4c031d1b0544
Ranbir Sinha <rsinha.u64@gmail.com>
2017-07-01 17:19:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from algo.nlpalgorithm import ReviewMiner
+from output.displayResult import Display
 
 def main():
     parser = argparse.ArgumentParser()
@@ -11,10 +12,13 @@ def main():
 
     #process the files for the "topic" in the reviews
     reviewAlgo = ReviewMiner(args.file, args.topic)
+    reviewAlgo.process()
 
-    #TODO
     #display the result - hotel with best rating on "topic" & other statistics
+    print 'Best Hotel: ', Display.hotelIDNameMap[Display.getBestHotel()]
+    print 'Topic Rating: ', Display.hotelScore[Display.getBestHotel()][0]
+    Display.displayAvgRating(Display.hotelScore[Display.getBestHotel()][1])
 
 if __name__ == '__main__':
-    #Usage: python main.py -f file1.json file2.json file3.json -t food
+    #Usage: python main.py -f reviews1.json reviews2.json reviews3.json reviews4.json reviews5.json -t food
     main()
